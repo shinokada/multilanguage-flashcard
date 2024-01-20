92c1749bb6f4988b7763c23b317a8d0c928a090b
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-20 09:06:51 +0100

    feat: flashcard page

--- src/routes/flashcard/+page.svelte
+++ src/routes/flashcard/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { twMerge } from 'tailwind-merge';
 	import { Label, Select, Button } from 'svelte-5-ui-lib';
 	import { randomNumberGenerator } from '$lib/utils.svelte';
-	const randomIndexFn = randomNumberGenerator(1, 5034, 50);
+	import { Flashcard, ArrowLeft, ArrowRight } from '$lib';
+	const randomIndexFn = randomNumberGenerator(1, 1933, 50);
 
-	let randomIndex = $state(100);
-	$inspect('randomIndex', randomIndex);
+	let randomIndex = $state(randomIndexFn());
+	// $inspect('randomIndex', randomIndex);
 
 	const languages = [
 		{ value: 'bn', name: 'বাংলা' },
@@ -36,37 +38,108 @@
 		{ value: 'zh-hant', name: '中文（繁體）' }
 	];
 
-    const random = ()=>{
-        randomIndex = randomIndexFn();
-    }
+	const random = async () => {
+		randomIndex = randomIndexFn();
+	};
 
-	let myLang = $state('en');
-	let targetLang = $state('nb');
-	$inspect('myLanguage: ', myLang, 'targeLanguage: ', targetLang);
+	let myLang = $state('ja');
+	let targetLang = $state('ko');
 	let selectedLanguage = $derived(languages.find((lang) => lang.value === myLang));
 	let targetLanguage = $derived(languages.find((lang) => lang.value === targetLang));
-let json
-    $effect( () => {
-    // Fetch and import the JSON data
-    const response =  import(`../../../node_modules/emojibase-data/${myLang}/data.json`);
-    // json = response.default;
-    console.log(response)
-  });
-    // let motherLang = $state(json.default[randomIndex])
-	// selectedLanguage = languages.find(lang => lang.value === myLanguage);
+	let lang1lang2 = $state(
+		'text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 opacity-100'
+	);
+	let lang2lang1 = $state(
+		'focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 me-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opacity-50'
+	);
+	let showCardBack = $state(false);
+	let showFront = $state('Show front');
+	let showBack = $state('Show back');
+	const toggleShowBack = () => (showCardBack = !showCardBack);
+
+	const nextWord = () => {
+		
+		showCardBack = false;
+		random();
+
+	};
 </script>
 
-<h1>Flashcard</h1>
-<h2>Let's learn {targetLanguage.name} using {selectedLanguage.name}</h2>
-<Select class="mt-2" items={languages} bind:value={myLang} />
-<Select class="mt-2" items={languages} bind:value={targetLang} />
+<div class="mt-15 flex flex-col items-center">
+	<h1>Multilanguage Flashcard</h1>
+	<h2>Let's learn {targetLanguage.name} using {selectedLanguage.name}</h2>
+    <div class="w-96 m-4">
+        <Select class="mt-2" items={languages} bind:value={myLang} onchange={random}/>
+        <Select class="mt-2" items={languages} bind:value={targetLang} onchange={random}/>
+    </div>
+	<!-- FLASHCARD -->
+	<div class="flip-box h-96 w-full bg-transparent md:w-2/3">
+		<div class="flip-box-inner" class:flip-it={showCardBack}>
+			<div class="relative h-full">
+				<div class='absolute inset-0 bg-red-700 p-4 text-gray-200 flex justify-center items-center'>
+					{#await import(`../../../node_modules/emojibase-data/${myLang}/data.json`) then json}
+						<p class="p-8 text-9xl">
+							{json.default[randomIndex].emoji}
+						</p>
+						<p class="mt-4 text-4xl">
+                            {json.default[randomIndex].label}
+                        </p>
+					{/await}
+				</div>
+				<div
+					class="absolute inset-0 flex p-4 items-center justify-center bg-blue-700 text-gray-200 opacity-0 {showCardBack
+						? 'opacity-100 [transform:rotateY(180deg)]'
+						: ''}"
+				>
+					{#await import(`../../../node_modules/emojibase-data/${targetLang}/data.json`) then json}
+						<p class="p-8 text-9xl">
+							{json.default[randomIndex].emoji}
+						</p>
+						<p class="mt-4 text-4xl">
+                            {json.default[randomIndex].label}
+                        </p>
+					{/await}
+				</div>
+			</div>
+		</div>
+	</div>
 
-<Button onclick={random}>Random {randomIndex}</Button>
+	<div class="flex space-x-4 pt-4">
+		<button onclick={toggleShowBack} class="inline-flex min-w-44 items-center bg-gray-300 p-4">
+			<ArrowLeft class="mr-4" />
+			{showCardBack ? showFront : showBack}
+		</button>
 
-{#await import(`../../../node_modules/emojibase-data/${myLang}/data.json`) then json}
-	<pre>{JSON.stringify(json.default[randomIndex], null, 2)}</pre>
-{/await}
+		<button class="inline-flex bg-gray-300 p-4 text-right" onclick={() => nextWord()}>
+			NEXT
+			<ArrowRight class="ml-4" />
+		</button>
+	</div>
+	<span class="right-full mt-4 hidden rounded bg-gray-900 px-2 py-1 text-white lg:inline-block">
+		Use ← to flip and → to next
+	</span>
+</div>
 
-{#await import(`../../../node_modules/emojibase-data/${targetLang}/data.json`) then json}
-	<pre>{JSON.stringify(json.default[randomIndex], null, 2)}</pre>
-{/await}
+<style>
+	.flip-box {
+		background-color: transparent;
+		/* width: 400px;
+		height: 300px; */
+		/* 		border: 1px solid #ddd; */
+		perspective: 1000px; /* Remove this if you don't want the 3D effect */
+	}
+	/* This container is needed to position the front and back side */
+	.flip-box-inner {
+		position: relative;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		transition: transform 0.8s;
+		transform-style: preserve-3d;
+	}
+
+	/* Do an horizontal flip on button click */
+	.flip-it {
+		transform: rotateY(180deg);
+	}
+</style>
