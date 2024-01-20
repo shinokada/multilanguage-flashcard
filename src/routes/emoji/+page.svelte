ce3fb55bf26ea30cf877ae08792290329ef45d10
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-20 11:09:01 +0100

    feat: add emoji page

---
+++ src/routes/emoji/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	// import emojis from 'emojibase-data/en/data.json';
+	import { Select } from 'svelte-5-ui-lib';
+	const languages = [
+		{ value: 'bn', name: 'বাংলা' },
+		{ value: 'da', name: 'Dansk' },
+		{ value: 'de', name: 'Deutsch' },
+		{ value: 'en', name: 'English' },
+		{ value: 'en-gb', name: 'English (UK)' },
+		{ value: 'es', name: 'Español' },
+		{ value: 'es-mx', name: 'Español (México)' },
+		{ value: 'et', name: 'Eesti keel' },
+		{ value: 'fi', name: 'Suomi' },
+		{ value: 'fr', name: 'Français' },
+		{ value: 'hi', name: 'हिन्दी' },
+		{ value: 'hu', name: 'Magyar' },
+		{ value: 'it', name: 'Italiano' },
+		{ value: 'ja', name: '日本語' },
+		{ value: 'ko', name: '한국어' },
+		{ value: 'lt', name: 'Lietuvių' },
+		{ value: 'ms', name: 'Bahasa Melayu' },
+		{ value: 'nb', name: 'Norsk Bokmål' },
+		{ value: 'nl', name: 'Nederlands' },
+		{ value: 'pl', name: 'Polski' },
+		{ value: 'pt', name: 'Português' },
+		{ value: 'ru', name: 'Русский' },
+		{ value: 'sv', name: 'Svenska' },
+		{ value: 'th', name: 'ไทย' },
+		{ value: 'uk', name: 'Українська' },
+		{ value: 'zh', name: '中文（简体）' },
+		{ value: 'zh-hant', name: '中文（繁體）' }
+	];
+
+	let myLang = $state('en');
+</script>
+
+<div class="mx-auto p-8 text-lg">
+	<h1>Emoji list</h1>
+
+	<Select class="my-4 w-64" items={languages} bind:value={myLang} />
+
+	<div class="grid grid-cols-4 gap-4">
+		{#await import(`../../../node_modules/emojibase-data/${myLang}/data.json`) then emojis}
+            {#each emojis.default as emoji}
+				<div>
+					<p>{emoji.label}: <span class="text-2xl">{emoji.emoji}</span></p>
+				</div>
+			{/each}
+		{/await}
+	</div>
+</div>
