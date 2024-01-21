2d47a1033cd56ca89631823f95bd3e95a8b772d0
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-21 16:08:28 +0100

    feat: add about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,4 @@
 <h1>About</h1>
 
+<p>This flashcard system supports a wide array of languages, allowing users to learn and practice vocabulary in their target language. From commonly spoken languages like English, Spanish, and Mandarin to niche languages, we've got you covered.</p>
+
