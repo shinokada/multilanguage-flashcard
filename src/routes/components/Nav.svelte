2739d0cf2b8ca8eb2f5cac0912bbb0888f783cba
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-01-21 16:03:45 +0100

    fix: update Nav ulClass font size

--- src/routes/components/Nav.svelte
+++ src/routes/components/Nav.svelte
@@ -8,7 +8,7 @@
     // let divClass = 'w-full ml-auto lg:block lg:w-auto lg:order-none';
     let divClass = "ml-auto w-full"
     let ulClass =
-    'flex flex-col py-3 lg:flex-row lg:my-0 text-sm order-1 font-medium gap-4 dark:lg:bg-transparent lg:bg-white lg:border-0';
+    'flex flex-col py-3 lg:flex-row lg:my-0 order-1 font-medium gap-4 dark:lg:bg-transparent lg:bg-white lg:border-0';
     let navClass = 'w-full divide-gray-200 border-gray-200 bg-white text-gray-500 dark:divide-gray-700 dark:border-gray-700 dark:bg-indigo-950 dark:text-gray-400 sm:px-4'
 	$effect(() => {
 		// this can be done adding nav.navStatus directly to DOM element
