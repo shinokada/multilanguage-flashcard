731c8a20eb431ac24e75e0808a081205fd4d2fd4
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-09-20 06:49:00 +0200

    chore: svelte@next, svelte-check and dependencies update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,8 +82,8 @@
 
   <div class="mb-4 w-96">
     <Label>Change your languages:</Label>
-    <Select selectClass="mt-2" items={languages} bind:value={myLang} onchange={random} />
-    <Select selectClass="mt-2" items={languages} bind:value={targetLang} onchange={random} />
+    <Select class="mt-2" items={languages} bind:value={myLang} onchange={random} />
+    <Select class="mt-2" items={languages} bind:value={targetLang} onchange={random} />
   </div>
   <h2>You are learning {targetLanguage?.name} using {selectedLanguage?.name}</h2>
   <!-- FLASHCARD -->
