cab3bc482b1fe10ace7d1b6716e568c09ecc2b0f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-09-23 10:53:21 +0200

    site: add sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,10 @@
+// /src/routes/sitemap.xml/+server.ts
+import * as sitemap from 'super-sitemap';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+  return await sitemap.response({
+    origin: 'https://multilanguage-flashcard.codewithshin.com',
+    excludeRoutePatterns: ['/dynamic/*', '/flashcard/*'],
+  });
+};
